fd68189492cf8304d0e2bf61418a9382e016b60d
Aditya raj <88555779+dryruffian@users.noreply.github.com>
2025-03-03 21:48:49 +0530

    fixed

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { Presentation, ExternalLink } from 'lucide-react';
 import { MagneticButton } from './Aboutme';
 
